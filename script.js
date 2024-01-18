8881be666fdd98ff8c69e4f61242769c0b17b18e
andi muh haikal lukman <andimuhhaikal79@gmail.com>
2024-01-18 23:08:42 +0800

    add function tweet

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ async function getQuote(url) {
   author.innerHTML = data.author;
 }
 getQuote(api_url);
+
+function tweet() {
+  window.open(
+    `https://twitter.com/intent/tweet?text=${quote.innerHTML} - ${author.innerHTML}`
+  );
+}
